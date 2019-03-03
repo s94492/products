5b04c732b2b53137a2eeec2a791934a0f169f152
WebberChen <s94492@gmail.com>
2019-03-03 22:52:35 +0800

    no3

--- products.py
+++ products.py
@@ -1,11 +1,28 @@
 product =[]
+with open("products.csv","r",encoding="utf8") as r :
+    for line in r :
+        if "商品,價格" in line :
+            continue
+        name, price = line.strip().split(",")
+        product.append([name, price])
+print(product)
+
+#
 while True :
     name = input("請輸入商品名稱:")
     if  name == "q" :
         break
-    price = input("請輸入價格:")
+    price = int(input("請輸入價格:"))
     product.append([name,price])
 print (product)
 
+#印出價錢
 for p in product:
     print (p[0],"價錢為:",p[1])
+
+# 寫入檔案
+with open("products.csv","w",encoding= "utf-8") as f :
+    f.write('商品,價格\n')
+    for p in product :
+        f.write(p[0]+","+str(p[1])+"\n")
+
