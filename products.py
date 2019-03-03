5e5649db759c8a69b3741e6be0d9ee6c138a8a0a
WebberChen <s94492@gmail.com>
2019-03-03 23:03:00 +0800

    iffile

--- products.py
+++ products.py
@@ -1,13 +1,19 @@
-product =[]
-with open("products.csv","r",encoding="utf8") as r :
-    for line in r :
-        if "商品,價格" in line :
-            continue
-        name, price = line.strip().split(",")
-        product.append([name, price])
-print(product)
+import os
+product = []
+#檢查檔案存在否
+if os.path.isfile('products.csv') :
+    print ("找到檔案了!!")
+    with open("products.csv", "r", encoding="utf8") as r:
+        for line in r:
+            if "商品,價格" in line:
+                continue
+            name, price = line.strip().split(",")
+            product.append([name, price])
+    print(product)
+else:
+    print("查無檔案")
 
-#
+#使用者輸入
 while True :
     name = input("請輸入商品名稱:")
     if  name == "q" :
