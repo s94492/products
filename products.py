b297a4538eb57056d1a5fdc889a953bfcfbc2068
WebberChen <s94492@gmail.com>
2019-03-04 15:31:52 +0800

    function

--- products.py
+++ products.py
@@ -1,34 +1,47 @@
 import os
-product = []
 #檢查檔案存在否
-if os.path.isfile('products.csv') :
-    print ("找到檔案了!!")
-    with open("products.csv", "r", encoding="utf8") as r:
+def chackfile(filename):
+    products = []
+    with open(filename, "r", encoding="utf8") as r:
         for line in r:
-            if "商品,價格" in line:
-                continue
-            name, price = line.strip().split(",")
-            product.append([name, price])
-    print(product)
-else:
-    print("查無檔案")
-
+             if "商品,價格" in line:
+                  continue
+        name, price = line.strip().split(",")
+        products.append([name, price])
+        print(products)
+    return products
 #使用者輸入
-while True :
-    name = input("請輸入商品名稱:")
-    if  name == "q" :
-        break
-    price = int(input("請輸入價格:"))
-    product.append([name,price])
-print (product)
-
+def userinput(products):
+    while True :
+        name = input("請輸入商品名稱:")
+        if  name == "q" :
+            break
+        price = int(input("請輸入價格:"))
+        products.append([name,price])
+    print (products)
+    return products
 #印出價錢
-for p in product:
-    print (p[0],"價錢為:",p[1])
-
+def printproducts(products):
+    for p in products:
+        print (p[0],"價錢為:",p[1])
 # 寫入檔案
-with open("products.csv","w",encoding= "utf-8") as f :
-    f.write('商品,價格\n')
-    for p in product :
-        f.write(p[0]+","+str(p[1])+"\n")
+def writeproducts(filename,products):
+    with open(filename,"w",encoding= "utf-8") as f :
+        f.write('商品,價格\n')
+        for p in products :
+            f.write(p[0]+","+str(p[1])+"\n")
 
+def main():
+    filename= "products.csv"
+    if os.path.isfile(filename) :
+        print("找到檔案了!!")
+        products = chackfile(filename)
+    else:
+        products = []
+        print('找不到檔案')
+
+    products = userinput(products)
+    printproducts(products)
+    writeproducts('products.csv',products)
+
+main()
